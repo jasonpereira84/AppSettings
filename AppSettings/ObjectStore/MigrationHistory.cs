39a679ee77e9b0821fab16dc4e4d145ead7d20ab
Jason Pereira <jasonpereira84@gmail.com>
2026-01-11 17:55:57 -0600

    Added MigrationHistory

---
+++ AppSettings/ObjectStore/MigrationHistory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JasonPereira84.AppSettings
+{
+    public partial class ObjectStore
+    {
+        public class MigrationHistory : Dictionary<String, DateTime> 
+        {   
+        }
+    }
+}
